a6b79c2adf44e138b04d06f21f0378c5bea28109
Dakspieth <jrb.games09@gmail.com>
2026-06-11 20:15:22 -0400

    added new way for multi d wall to work but csan't think of any puzzles. I'm definitely going about making this game wrong

--- Assets/Scripts/MultiDWall.cs
+++ Assets/Scripts/MultiDWall.cs
@@ -1,11 +1,21 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MultiDWall : MonoBehaviour
 {
+    [Header("Time-Based")]
     [SerializeField]
+    public bool timeBased = true;
     public float offset, timeOn, timeOff;
     public bool on = true;
+    
+    [Header("State-Based")]
+    public List<bool> states = new List<bool>();
+    public float stateTime;
+    int currState = 0;
+
     float timer = 0;
+
     SpriteRenderer sr;
     BoxCollider2D bc;
 
@@ -20,26 +30,48 @@ public class MultiDWall : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player"); 
         resetScreen = GameObject.FindGameObjectWithTag("ResetScreen").GetComponent<ResetScreen>();
         timer = -offset;
+        currState = states.Count-1;
     }
 
     void Update()
     {
-        sr.enabled = on;
-        bc.enabled = on;
-        if(on && timer < timeOn)
+        if (timeBased)
         {
-            timer += Time.deltaTime;
-        } else if (!on && timer < timeOff)
-        {
-            timer += Time.deltaTime;
-        } else
-        {
-            on = !on;
-            timer = 0;
-            if(Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) < player.transform.localScale.x/2)
+            sr.enabled = on;
+            bc.enabled = on;
+            if(on && timer < timeOn)
             {
-                resetScreen.ReloadScreen();
-                print("die");
+                timer += Time.deltaTime;
+            } else if (!on && timer < timeOff)
+            {
+                timer += Time.deltaTime;
+            } else
+            {
+                on = !on;
+                timer = 0;
+                if(Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) < player.transform.localScale.x/2)
+                {
+                    resetScreen.ReloadScreen();
+                    print("die");
+                }
+            }
+        } else {
+            if(timer > stateTime)
+            {
+                    // checks if the next state is different than the current
+                if(states [currState] != states[currState+1<states.Count ? currState+1 : 0] && Mathf.Abs(player.transform.position.x - gameObject.transform.position.x) < player.transform.localScale.x/2)
+                {
+                    resetScreen.ReloadScreen();
+                    print("die");
+                }
+                currState += currState+1 < states.Count ? 1 : -currState; // increments currstate if in range, else set to 0
+                timer = 0;
+                sr.enabled = states[currState];
+                bc.enabled = states[currState];
+                
+            } else
+            {
+                timer += Time.deltaTime;
             }
         }
     }
