ec3a051fb9a456dc9d45064f00b5856edb08d230
Dakspieth <jrb.games09@gmail.com>
2026-06-16 22:40:34 -0400

    added coyote time and brute force fixed a wall jump bug

--- Assets/Scripts/TwoDPlayerMove.cs
+++ Assets/Scripts/TwoDPlayerMove.cs
@@ -9,16 +9,19 @@ public class TwoDPlayerMove : MonoBehaviour
     public float jumpTime; // how long to stay in air while holding jump
         float baseJumpTime;
         bool readJump = true;
+    public float coyoteTime;
+        float baseCoyoteTime;
     public InputAction jumpControl;
     
     Rigidbody2D rb;
     float moveDir;
-    bool grounded;
+    bool groundCollided;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         baseJumpTime = jumpTime;
+        baseCoyoteTime = coyoteTime;
     }
 
     void OnEnable()
@@ -33,17 +36,31 @@ public class TwoDPlayerMove : MonoBehaviour
     }
     void FixedUpdate()
     {
-        grounded = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2), // arg 1
-                                        new Vector2(transform.localScale.x-0.01f, 0.1f), 0f, LayerMask.GetMask("Ground"));    // arg 2
+        groundCollided = Physics2D.OverlapBox(new Vector2(transform.position.x, transform.position.y - transform.localScale.y / 2),
+                                              new Vector2(transform.localScale.x-0.02f, 0.1f), 0f, LayerMask.GetMask("Ground"));
+        if(groundCollided) {
+            coyoteTime = baseCoyoteTime;
+        } else {
+            coyoteTime -= Time.deltaTime;
+        }
 
         moveDir = moveControl.ReadValue<float>(); // left right
-        if(grounded && jumpControl.ReadValue<float>() == 1 && readJump) {
+
+        
+
+            // instead of grounded bool
+        if(coyoteTime>0 && jumpControl.ReadValue<float>() == 1 && readJump) {
             jumpTime = baseJumpTime;
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            readJump = false;
-        } else if(!grounded && jumpControl.ReadValue<float>() == 1 && jumpTime > 0) {
+            //readJump = false;
+        } else if(!groundCollided && jumpControl.ReadValue<float>() == 1 && jumpTime > 0) {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        } else if(jumpControl.ReadValue<float>() == 0) {
+         }// else if(jumpControl.ReadValue<float>() == 0) {
+        //     readJump = true;
+        // }
+        if(jumpControl.ReadValue<float>() == 1) {
+            readJump = false;
+        } else {
             readJump = true;
         }
 
